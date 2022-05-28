d15524478d90c0f060a00940f3df1680e1c90318
hasanboy <metinnazif77@gmail.com>
2022-05-28 17:29:03 +0500

    Create Application prperties

--- src/main/java/uz/isystem/productApplication/controller/hasanboy/BrantController.java
+++ src/main/java/uz/isystem/productApplication/controller/hasanboy/BrantController.java
@@ -1,4 +1,11 @@
 package uz.isystem.productApplication.controller.hasanboy;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/brant")
+@AllArgsConstructor
 public class BrantController {
 }
--- src/main/java/uz/isystem/productApplication/controller/hasanboy/OrderItemController.java
+++ src/main/java/uz/isystem/productApplication/controller/hasanboy/OrderItemController.java
@@ -1,4 +1,11 @@
 package uz.isystem.productApplication.controller.hasanboy;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order/item")
+@AllArgsConstructor
 public class OrderItemController {
 }
